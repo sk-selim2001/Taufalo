337466c00153375c6e31e33891d125220e5be252
sk-selim2001 <124250567+sk-selim2001@users.noreply.github.com>
2023-02-02 16:32:18 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// // Testimonials Section
+// const wrapper = document.querySelector('.testimonials-wrapper');
+// const indicators = [...document.querySelectorAll('.testimonials-indicators button')];
+
+// let currentTestimonial = 0; // Default 0
+
+// indicators.forEach((item, i) => {
+//     item.addEventListener('click', () => {
+//         indicators[currentTestimonial].classList.remove('active');
+//         wrapper.style.marginLeft = `-${100 * i}%`;
+//         item.classList.add('active');
+//         currentTestimonial = i;
+//     })
+// })
+// // Testimonials Section end
